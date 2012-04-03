a08fffa122fa424559a069fab564c83bd7656a72
Josh @ Dreamland <JoshV@zoominternet.net>
2012-04-03 18:22:27 -0400

    Lays out the foundation for the IOFormat class and the ObjectWriter class. Commits first "complete" JavaDoc comments. Implements a formatDate() function following specification OB1.7.

---
+++ src/IOFormat.java
@@ -0,0 +1,147 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Formating class to format objects and check the formatting of objects.
+ * 
+ * @date 2012-04-03 Tuesday 02:32PM
+ * @author Josh Ventura
+ */
+public class IOFormat {
+	/**
+	 * Check a label for validity against the URBAN specification, V1.0.
+	 * 
+	 * @date 2012-04-03 Tuesday 02:33PM
+	 * @modifications UNMODIFIED
+	 * @tested UNTESTED
+	 * @errors This function facilitates reporting errors. It does not generate
+	 *         them.
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param label
+	 *            The label to check for validity.
+	 * @return Returns whether this label is valid according to the
+	 *         specification.
+	 * @specRef S2.1
+	 */
+	public static boolean isValidLabel(String label) {
+		for (int i = 0; i < label.length(); ++i)
+			switch (label.charAt(i)) {
+			case ':':
+				return false;
+			case ';':
+				return false;
+			case ' ':
+				return false;
+			case '\t':
+				return false;
+			}
+		return true;
+	}
+
+	/**
+	 * Format an integer to a zero-padded decimal representation.
+	 * 
+	 * @author Josh Ventura
+	 * @modifications UNMODIFIED
+	 * @tested 2012-04-03 Tuesday 03:10PM: Tested the decimal number 1337,
+	 *         printed with 10, 5, 4, 2, 1, and 0 digits of allowance.
+	 * 
+	 *         2012-04-03 Tuesday 05:54PM: Tested the decimal number 9001, to
+	 *         verify there was no issue with 0-result modulus.
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param number
+	 *            A number between 0 and (10^(digits+1)-1).
+	 * @param digits
+	 *            The number of digits to return.
+	 * @return A byte[] containing the integer in decimal notation.
+	 */
+	public static byte[] formatInteger(int number, int digits) {
+		byte res[] = new byte[digits];
+		for (int i = 0; i < digits; ++i)
+			res[i] = '0';
+		for (int i = digits - 1; i >= 0 && number > 0; --i) {
+			res[i] = (byte) ('0' + (number % 10));
+			number /= 10;
+		}
+		return res;
+	}
+
+	/**
+	 * An array of hex nybbles for quick construction of hex string
+	 * representations.
+	 */
+	private static byte[] hexNybbles = { '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+	/**
+	 * Format an integer to a zero-padded hexadecimal representation.
+	 * 
+	 * @author Josh Ventura
+	 * @modifications UNMODIFIED
+	 * @tested 2012-04-03 Tuesday 03:10PM: Tested the decimal number 1337,
+	 *         printed with 10, 4, 3, 2, 1, and 0 digits of allowance.
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param number
+	 *            A number between 0 and (10^(digits+1)-1).
+	 * @param digits
+	 *            The number of digits to return.
+	 * @return A byte[] containing the integer in decimal notation.
+	 */
+	public static byte[] formatHexInteger(int number, int digits) {
+		byte res[] = new byte[digits];
+		for (int i = 0; i < digits; ++i)
+			res[i] = '0';
+		for (int i = digits - 1; i >= 0 && number > 0; --i) {
+			res[i] = hexNybbles[number % 16];
+			number /= 16;
+		}
+		return res;
+	}
+
+	/**
+	 * Format a date as "YYYYDDD,HH:MM:SS".
+	 * 
+	 * @author Josh Ventura
+	 * @modifications UNMODIFIED
+	 * @tested 2012-04-03 Tuesday 05:39PM: Tested with a new Date object
+	 *         representing the current date and time.
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param date
+	 *            The date to format.
+	 * @return A byte[] containing a representation of the date, according to
+	 *         specification.
+	 * @specRef OB1.7
+	 */
+	public static byte[] formatDate(Date date) {
+		byte[] res = new byte[16];
+		byte[] append;
+		int appat = 0;
+		GregorianCalendar calendar = (GregorianCalendar) GregorianCalendar
+				.getInstance();
+		calendar.setTime(date);
+		append = formatInteger(calendar.get(Calendar.YEAR), 4);
+		for (int i = 0; i < 4; ++i)
+			res[appat++] = append[i];
+		append = formatInteger(calendar.get(Calendar.DAY_OF_YEAR), 3);
+		for (int i = 0; i < 3; ++i)
+			res[appat++] = append[i];
+		append = formatInteger(calendar.get(Calendar.HOUR_OF_DAY), 2);
+		res[appat++] = ',';
+		for (int i = 0; i < 2; ++i)
+			res[appat++] = append[i];
+		res[appat++] = ':';
+		append = formatInteger(calendar.get(Calendar.MINUTE), 2);
+		for (int i = 0; i < 2; ++i)
+			res[appat++] = append[i];
+		res[appat++] = ':';
+		append = formatInteger(calendar.get(Calendar.SECOND), 2);
+		for (int i = 0; i < 2; ++i)
+			res[appat++] = append[i];
+		return res;
+	}
+}
---
+++ src/ObjectWriter.java
@@ -0,0 +1,103 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
+
+/**
+ * I/O class for generated object files.
+ * 
+ * @author Josh Ventura
+ */
+public class ObjectWriter {
+	/**
+	 * @date 2012-04-03 Tuesday 02:33PM Check a label for validity against the
+	 *       URBAN specification, V1.0.
+	 * @modifications UNMODIFIED
+	 * @tested UNTESTED
+	 * @errors This function facilitates reporting errors. It does not
+	 *                  generate them.
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param out
+	 *            The stream to which the header will be written.
+	 * @param progname
+	 *            The name of the program being assembled, which must be a valid
+	 *            label.
+	 * @param programAddr
+	 *            Assembler assigned program load address, per #OBJ1.3.
+	 * @param moduleLength
+	 *            The total length of the module.
+	 * @param execStart
+	 *            The starting address of execution.
+	 * @param asmblrVersion
+	 *            The version number of the assembler.
+	 * @throws IOException
+	 *             Throws an IOException if data cannot be written.
+	 * @throws Exception
+	 *             Throws Exception if the program name is not a valid label.
+	 * @specRef OB1
+	 * @specRef OB1.1
+	 * @specRef OB1.2
+	 * @specRef OB1.3
+	 * @specRef OB1.4
+	 * @specRef OB1.5
+	 * @specRef OB1.6
+	 * @specRef OB1.7
+	 * @specRef OB1.8
+	 * @specRef OB1.9
+	 * @specRef OB1.18
+	 * @specRef OB1.19
+	 * @specRef OB1.20
+	 * @specRef OB1.21
+	 * @specRef S2.1
+	 */
+	public void writeHeaderRecord(OutputStream out, String progname,
+			int programAddr, int moduleLength, int execStart, int asmblrVersion)
+			throws IOException, Exception {
+		// ===================================================================
+		// ==== Check formatting. ============================================
+		// ===================================================================
+
+		if (progname.length() > 32 || IOFormat.isValidLabel(progname))
+			throw new Exception("Program name is not in valid label format");
+
+		// ===================================================================
+		// ==== Write header. ================================================
+		// ===================================================================
+
+		// OB1.1: 'H' - Single character
+		out.write('H');
+		// OB1.2: ':' - Single character
+		out.write(':');
+		// OB1.3: `Program name` - String of 1 to 32 characters and numbers
+		// meeting syntax rules for a label
+		out.write(progname.getBytes());
+		// OB1.2: ':' - Single character
+		out.write(':');
+		// OB1.3: `Assembler-assigned program load address` - 4 digit hex number HHHH
+		out.write(IOFormat.formatInteger(programAddr, 4));
+		// OB1.2: ':' - Single character
+		out.write(':');
+		// OB1.3: `Total Module length` - 4 nybble hex number (0 to 03FF)
+		out.write(IOFormat.formatInteger(moduleLength, 4));
+		// OB1.4: ':' - Single character
+		out.write(':');
+		// OB1.5: `Execution start address` - 4 nybble hex number (0 to 03FF)
+		out.write(IOFormat.formatInteger(execStart, 4));
+		// OB1.6: ':' - Single character
+		out.write(':');
+		// OB1.7: `Date/time of Assembly` - "YYYYDDD,HH:MM:SS"
+		out.write(IOFormat.formatDate(new Date()));
+		// OB1.8: ':' - Single character
+		out.write(':');
+		// OB1.9: `Assembler version number` - 4 digit integer
+		out.write(IOFormat.formatInteger(asmblrVersion, 4));
+		// OB1.18: ':' - Single character
+		out.write(':');
+		// OB1.19: "URBAN-ASM" - 9 character string
+		out.write("URBAN-ASM".getBytes());
+		// OB1.20: ':' - Single character
+		out.write(':');
+		// OB1.21: `Program name` - See OB1.3
+		out.write(progname.getBytes());
+	}
+}
