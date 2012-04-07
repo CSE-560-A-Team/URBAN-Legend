a3ce0cd3419fcd9aebdaf0eff4fb33e02e867637
Josh @ Dreamland <JoshV@zoominternet.net>
2012-04-07 16:42:03 -0400

    Adds a skeleton class for each instruction, and the file that generates them, as well as the list of calls to getInstance() in main.

---
+++ src/instructions/USI_DMP.java
@@ -0,0 +1,92 @@
+package instructions;
+
+import assemblernator.Instruction;
+
+/**
+ * The DMP instruction.
+ * 
+ * @author Generate.java
+ * @date Apr 07, 2012; 04:41:01
+ * @specRef C1
+ */
+public class USI_DMP extends Instruction {
+	/**
+	 * The operation identifier of this instruction; while comments should not
+	 * be treated as an instruction, specification says they must be included in
+	 * the user report. Hence, we will simply give this class a semicolon as its
+	 * instruction ID.
+	 */
+	private static final String opId = "DMP";
+
+	/** This instruction's identifying opcode. */
+	private static final int opCode = 0x0000003E; // 0b11111000000000000000000000000000
+
+	/** The static instance for this instruction. */
+	static USI_DMP staticInstance = new USI_DMP(true);
+
+	/** @see assemblernator.Instruction#getWordCount() */
+	@Override public int getWordCount() {
+		return 1;
+	}
+
+	/** @see assemblernator.Instruction#check() */
+	@Override public boolean check() {
+		return false; // TODO: IMPLEMENT
+	}
+
+	/** @see assemblernator.Instruction#assemble() */
+	@Override public int[] assemble() {
+		return null; // TODO: IMPLEMENT
+	}
+
+	/** @see assemblernator.Instruction#execute(int) */
+	@Override public void execute(int instruction) {
+		// TODO: IMPLEMENT
+	}
+
+	// =========================================================
+	// === Redundant code ======================================
+	// =========================================================
+	// === This code's the same in all instruction classes, ====
+	// === But Java lacks the mechanism to allow stuffing it ===
+	// === in super() where it belongs. ========================
+	// =========================================================
+
+	/**
+	 * @see Instruction
+	 * @return The static instance of this instruction.
+	 */
+	public static Instruction getInstance() {
+		return staticInstance;
+	}
+
+	/** @see assemblernator.Instruction#getOpId() */
+	@Override public String getOpId() {
+		return opId;
+	}
+
+	/** @see assemblernator.Instruction#getOpcode() */
+	@Override public int getOpcode() {
+		return opCode;
+	}
+
+	/** @see assemblernator.Instruction#getNewInstance() */
+	@Override public Instruction getNewInstance() {
+		return new USI_DMP();
+	}
+
+	/**
+	 * Calls the Instance(String,int) constructor to track this instruction.
+	 * 
+	 * @param ignored
+	 *            Unused parameter; used to distinguish the constructor for the
+	 *            static instance.
+	 */
+	private USI_DMP(boolean ignored) {
+		super(opId, opCode);
+	}
+
+	/** Default constructor; does nothing. */
+	private USI_DMP() {}
+}
+
---
+++ src/instructions/USI_EQU.java
@@ -0,0 +1,92 @@
+package instructions;
+
+import assemblernator.Instruction;
+
+/**
+ * The EQU instruction.
+ * 
+ * @author Generate.java
+ * @date Apr 07, 2012; 04:41:01
+ * @specRef D3
+ */
+public class USI_EQU extends Instruction {
+	/**
+	 * The operation identifier of this instruction; while comments should not
+	 * be treated as an instruction, specification says they must be included in
+	 * the user report. Hence, we will simply give this class a semicolon as its
+	 * instruction ID.
+	 */
+	private static final String opId = "EQU";
+
+	/** This instruction's identifying opcode. */
+	private static final int opCode = 0xFFFFFFFF; // This instruction doesn't have an opcode.
+
+	/** The static instance for this instruction. */
+	static USI_EQU staticInstance = new USI_EQU(true);
+
+	/** @see assemblernator.Instruction#getWordCount() */
+	@Override public int getWordCount() {
+		return 0;
+	}
+
+	/** @see assemblernator.Instruction#check() */
+	@Override public boolean check() {
+		return false; // TODO: IMPLEMENT
+	}
+
+	/** @see assemblernator.Instruction#assemble() */
+	@Override public int[] assemble() {
+		return null; // TODO: IMPLEMENT
+	}
+
+	/** @see assemblernator.Instruction#execute(int) */
+	@Override public void execute(int instruction) {
+		// TODO: IMPLEMENT
+	}
+
+	// =========================================================
+	// === Redundant code ======================================
+	// =========================================================
+	// === This code's the same in all instruction classes, ====
+	// === But Java lacks the mechanism to allow stuffing it ===
+	// === in super() where it belongs. ========================
+	// =========================================================
+
+	/**
+	 * @see Instruction
+	 * @return The static instance of this instruction.
+	 */
+	public static Instruction getInstance() {
+		return staticInstance;
+	}
+
+	/** @see assemblernator.Instruction#getOpId() */
+	@Override public String getOpId() {
+		return opId;
+	}
+
+	/** @see assemblernator.Instruction#getOpcode() */
+	@Override public int getOpcode() {
+		return opCode;
+	}
+
+	/** @see assemblernator.Instruction#getNewInstance() */
+	@Override public Instruction getNewInstance() {
+		return new USI_EQU();
+	}
+
+	/**
+	 * Calls the Instance(String,int) constructor to track this instruction.
+	 * 
+	 * @param ignored
+	 *            Unused parameter; used to distinguish the constructor for the
+	 *            static instance.
+	 */
+	private USI_EQU(boolean ignored) {
+		super(opId, opCode);
+	}
+
+	/** Default constructor; does nothing. */
+	private USI_EQU() {}
+}
+
