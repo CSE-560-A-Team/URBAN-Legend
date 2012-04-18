b0bd7c6a627ee12b00a815e71559ae5f8bac1884
Noah Torrance <torrance.8@buckeyemail.osu.edu>
2012-04-18 15:01:44 -0400

    got rid of hlt warning

--- src/instructions/USI_HLT.java
+++ src/instructions/USI_HLT.java
@@ -4,7 +4,6 @@ import static assemblernator.ErrorReporting.makeError;
 import static assemblernator.OperandChecker.isValidConstant;
 import assemblernator.AbstractInstruction;
 import assemblernator.ErrorReporting.ErrorHandler;
-import assemblernator.Instruction.ConstantRange;
 import assemblernator.IOFormat;
 import assemblernator.Instruction;
 import assemblernator.Module;
