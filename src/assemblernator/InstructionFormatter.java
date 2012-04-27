625a511f3594ee9100f424c3d4b6253df0f8111e
Noah Torrance <torrance.8@buckeyemail.osu.edu>
2012-04-27 18:17:29 -0400

    added formatter

---
+++ src/assemblernator/InstructionFormatter.java
@@ -0,0 +1,68 @@
+package assemblernator;
+
+/**
+ * 
+ * @author Noah
+ * @date Apr 27, 2012; 6:15:53 PM
+ */
+public class InstructionFormatter {
+	public static int [] formatOther(Instruction instr) {
+		return new int[1];
+	}
+	
+	/**
+	 * 
+	 * @author Noah
+	 * @date Apr 27, 2012; 6:17:09 PM
+	 * @modified UNMODIFIED
+	 * @tested UNTESTED
+	 * @errors NO ERRORS REPORTED
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param instr
+	 * @return
+	 * @specRef N/A
+	 */
+	public static int [] formatHaltDump(Instruction instr) {
+		return new int[1];
+	}
+	
+	public static int [] formatClear(Instruction instr) {
+		return new int[1];
+	}
+	
+	/**
+	 * 
+	 * @author Noah
+	 * @date Apr 27, 2012; 6:16:53 PM
+	 * @modified UNMODIFIED
+	 * @tested UNTESTED
+	 * @errors NO ERRORS REPORTED
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param instr
+	 * @return
+	 * @specRef N/A
+	 */
+	public static int [] formatInput(Instruction instr) {
+		
+		return new int[1];
+	}
+	
+	/**
+	 * 
+	 * @author Noah
+	 * @date Apr 27, 2012; 6:16:56 PM
+	 * @modified UNMODIFIED
+	 * @tested UNTESTED
+	 * @errors NO ERRORS REPORTED
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param instr
+	 * @return
+	 * @specRef N/A
+	 */
+	public static int [] formatOutput(Instruction instr) {
+		return new int[1];
+	}
+}
