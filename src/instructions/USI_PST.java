b23f12ec64d50c41ab026fc15de01b3fbb3d3c94
Noah Torrance <torrance.8@buckeyemail.osu.edu>
2012-04-27 20:26:40 -0400

    added assemble implementation.

--- src/instructions/USI_PST.java
+++ src/instructions/USI_PST.java
@@ -1,6 +1,7 @@
 package instructions;
 
 import static assemblernator.ErrorReporting.makeError;
+import static assemblernator.InstructionFormatter.formatOther;
 import assemblernator.AbstractInstruction;
 import assemblernator.ErrorReporting.ErrorHandler;
 import assemblernator.Instruction;
@@ -124,7 +125,7 @@ public class USI_PST extends AbstractInstruction {
 
 	/** @see assemblernator.Instruction#assemble() */
 	@Override public int[] assemble() {
-		return null; // TODO: IMPLEMENT
+		return formatOther(this);
 	}
 
 	/** @see assemblernator.Instruction#execute(int) */
