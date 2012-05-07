16ca2636a21ea3e44dcac646e75cf92136466483
Noah Torrance <torrance.8@buckeyemail.osu.edu>
2012-05-06 23:44:37 -0400

    added to writeObjectFile, and fixed bug in Instruction toString

--- src/assemblernator/Assembler.java
+++ src/assemblernator/Assembler.java
@@ -308,7 +308,6 @@ public class Assembler {
 		// Pass two
 		for (Instruction i : module.assembly) {
 			valid = i.check(i.getHErr(hErr), module);
-			
 			if(valid) {
 				module.execStartAddr = execStartAddr;
 				i.assembled = i.assemble(); //for now.  should replace all assemble w/ directly changing self's field.
