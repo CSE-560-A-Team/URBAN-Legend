6cad7bd728f6dfa0c3e04a2486009ac49a2fd06e
Ratul Khosla <ratulkhosla5@gmail.com>
2012-05-07 23:47:23 -0400

    added BitLocation to RET..

--- src/instructions/USI_RET.java
+++ src/instructions/USI_RET.java
@@ -7,6 +7,7 @@ import assemblernator.ErrorReporting.ErrorHandler;
 import assemblernator.Instruction;
 import assemblernator.Module;
 import assemblernator.Module.Value;
+import assemblernator.Module.Value.BitLocation;
 import assemblernator.OperandChecker;
 
 /**
@@ -52,7 +53,7 @@ public class USI_RET extends AbstractInstruction {
 				dest = "DM";
 				//range check
 				Operand o = getOperandData("DM");
-				Value constantSize = module.evaluate(o.expression, true, hErr, this,
+				Value constantSize = module.evaluate(o.expression, true, BitLocation.Address,  hErr, this,
 						o.valueStartPosition);
 				this.getOperandData("DM").value = constantSize;
 				isValid = OperandChecker.isValidMem(constantSize.value);
@@ -67,13 +68,13 @@ public class USI_RET extends AbstractInstruction {
 				dest="DMDX";
 				//range check
 				Operand o1 = getOperandData("DX");
-				Value constantSize1 = module.evaluate(o1.expression, false, hErr, this,
+				Value constantSize1 = module.evaluate(o1.expression, false, BitLocation.Other, hErr, this,
 						o1.valueStartPosition);
 				this.getOperandData("DX").value = constantSize1;
 				isValid = OperandChecker.isValidIndex(constantSize1.value);
 				if(!isValid) hErr.reportError(makeError("OORidxReg", "DX", this.getOpId()), this.lineNum, -1);
 				Operand o2 = getOperandData("DM");
-				Value constantSize2 = module.evaluate(o2.expression, true, hErr, this,
+				Value constantSize2 = module.evaluate(o2.expression, true, BitLocation.Address, hErr, this,
 						o2.valueStartPosition);
 				this.getOperandData("DM").value = constantSize2;
 				isValid = OperandChecker.isValidMem(constantSize2.value);
