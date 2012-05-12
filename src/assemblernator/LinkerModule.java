fc9dc2901a61cc0f57e269a8b8b0d24c7245af40
Eric Smith <eric44095@yahoo.com>
2012-05-12 16:42:50 -0400

    adding shit

--- src/assemblernator/LinkerModule.java
+++ src/assemblernator/LinkerModule.java
@@ -8,21 +8,30 @@ import java.io.InputStream;
  * @date May 12, 2012; 3:59:15 PM
  */
 public class LinkerModule {
+
+	public static class textRecord{
+		
+		
+	}
+	public static class modRecords{
+		
+		
+	}
 	/**
 	 * 
 	 * @author Eric
-	 * @date May 12, 2012; 3:59:22 PM
-	 * @modified UNMODIFIED
+	 * @date May 12, 2012; 4:15:37 PM
+	 * @modified May 12, 2012; 4:16:37 PM
 	 * @tested UNTESTED
 	 * @errors NO ERRORS REPORTED
 	 * @codingStandards Awaiting signature
 	 * @testingStandards Awaiting signature
-	 * @param in
-	 * @return
+	 * @param in the ObjectFile being read in from input
 	 * @specRef N/A
 	 */
-	public LinkerModule readObjectFile(InputStream in){
-		return null;	
+	public void readObjectFile(InputStream in){
+		
+
 	}
 	
 	
