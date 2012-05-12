09342bec8ee35f1025434c1c0ef89be136e54957
Eric Smith <eric44095@yahoo.com>
2012-05-12 16:00:17 -0400

    Made LinkerModule and readObjectFile function skeleton inside it

---
+++ src/assemblernator/LinkerModule.java
@@ -0,0 +1,29 @@
+package assemblernator;
+
+import java.io.InputStream;
+
+/**
+ * 
+ * @author Eric
+ * @date May 12, 2012; 3:59:15 PM
+ */
+public class LinkerModule {
+	/**
+	 * 
+	 * @author Eric
+	 * @date May 12, 2012; 3:59:22 PM
+	 * @modified UNMODIFIED
+	 * @tested UNTESTED
+	 * @errors NO ERRORS REPORTED
+	 * @codingStandards Awaiting signature
+	 * @testingStandards Awaiting signature
+	 * @param in
+	 * @return
+	 * @specRef N/A
+	 */
+	public LinkerModule readObjectFile(InputStream in){
+		return null;	
+	}
+	
+	
+}
