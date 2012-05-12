c044fa715ee3511924efef92bb4fa409986c3ed4
Josh @ Dreamland <JoshV@zoominternet.net>
2012-05-12 18:13:03 -0400

    Makes members of Deformatter public, as appropriate.

--- src/simulanator/Deformatter.java
+++ src/simulanator/Deformatter.java
@@ -9,7 +9,7 @@ public class Deformatter {
 	 * @author Josh Ventura
 	 * @date May 12, 2012; 5:42:34 PM
 	 */
-	enum Location {
+	public enum Location {
 		/** A normal register. */
 		REGISTER,
 		/** An index register. */
@@ -22,19 +22,29 @@ public class Deformatter {
 	 * @author Josh Ventura
 	 * @date May 12, 2012; 5:42:31 PM
 	 */
-	class OpcodeBreakdownOther {
+	public class OpcodeBreakdownOther {
 		/** True if the data field is a literal, false if it is an address. */
-		boolean literal;
+		public boolean literal;
 		/** The kind of the source. */
-		Location sourceKind;
+		public Location sourceKind;
 		/** The source register index or memory address. */
-		int source;
+		public int source;
 		/** True if the data field is a literal, false if it is an address. */
-		Location destKind;
+		public Location destKind;
 		/** The destination register index or memory address. */
-		int destination;
+		public int destination;
 	}
-
+	
+	/** The bit indicating if the data is a literal. */
+	private final int LITERALBIT = 0x01000000;
+	/** The bit indicating if the source is not a regular register. */
+	private final int SOURCEKBIT = 0x00800000;
+	/** The bitmask of the source register index. */
+	private final int SOURCEBITS = 0x00700000;
+	/** The bit indicating if the dest is not a regular register. */
+	private final int DESTINKBIT = 0x00080000;
+	/** The bitmask of the dest register index. */
+	private final int DESTINBITS = 0x00070000;
 	/**
 	 * @author Josh Ventura
 	 * @date May 12, 2012; 5:54:26 PM
@@ -47,19 +57,19 @@ public class Deformatter {
 	 * @return The breakdown of this instruction as formatOther.
 	 * @specRef N/A
 	 */
-	OpcodeBreakdownOther breakDownOther(int instruction) {
+	public OpcodeBreakdownOther breakDownOther(int instruction) {
 		OpcodeBreakdownOther res = new OpcodeBreakdownOther();
-		res.literal = (instruction & 0x01000000) != 0;
-		res.sourceKind = ((instruction & 0x00800000) == 0) ? Location.REGISTER
-				: ((instruction & 0x00700000) == 0) ? Location.MEMORY
+		res.literal = (instruction & LITERALBIT) != 0;
+		res.sourceKind = ((instruction & SOURCEKBIT) == 0) ? Location.REGISTER
+				: ((instruction & SOURCEBITS) == 0) ? Location.MEMORY
 						: Location.INDEXREGISTER;
 		if (res.sourceKind == Location.MEMORY)
 		{
-			//res.source = instruction & 
+			res.source = instruction & res.destination;
 		}
 
-		res.destKind = ((instruction & 0x00080000) == 0) ? Location.REGISTER
-				: ((instruction & 0x00070000) == 0) ? Location.MEMORY
+		res.destKind = ((instruction & DESTINKBIT) == 0) ? Location.REGISTER
+				: ((instruction & DESTINBITS) == 0) ? Location.MEMORY
 						: Location.INDEXREGISTER;
 
 		return res;
